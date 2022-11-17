06ad0b8d4c02043b557863e8bfc6843fd70816c1
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-17 08:35:45 +0200

    main

--- js/code.js
+++ js/code.js
@@ -236,12 +236,11 @@
 // let names = ['Matthew', 'John', 'Camryn', 'Faeez', 'Daniel', 'Lazola'];
 // let emails = ['matt@Corpmail.com','john12@Corpmail.com','camryn0000@Corpmail.com','fa33z@Corpmail.com','dan13l@Corpmail.com','laz@Corpmail.com'];
 
-// function checkCredentials(name, email, userName, userID) {
-//     if(userName && (userID.include) && (email == emails[0, 1, 2, 3, 4, 5]) && firstName == names[0, 1, 2, 3, 4, 5]){
+// function checkCredentials(names, email, userName, userID) {
+//     if(userName && (userID == iDs[12345678900, 12345678901, 12345678902, 12345678903]) && (email == emails[0, 1, 2, 3, 4, 5]) && firstName == names[0, 1, 2, 3, 4, 5]){
 //         document.write('Access Granted');
 //     }else {
 //         document.write('Access Denied');
 //     }
 // }
-// checkCredentials(name, email, userName, userID);
-// =========================================================
+// checkCredentials(names, email, userName, userID);
